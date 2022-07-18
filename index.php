efd539badc317007147da07c6207c3ac8689a142
Reni Kuswulandari <202020271@mhs.udb.ac.id>
2022-07-18 15:39:16 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<center><h2>Welcome To Rekap Data Karyawan</h2>
+	<center><p>Customer Service PT.TRIBER INDONESIA</p>
+	<center><img src ="perusahaan.png.jpeg" width="900" height="400"></center>
+</div>
+<p> Diskripsi Perusahaan:</p>
+<p> PT. Triber Indonesia adalah PT yang menjual paket usaha mulai dari 2 jutaan yang sudah siap jualan. 
+<p> Dengan harga 2 jutaan akan mendapatkan Booth,Xbanner,Cup + Free Bahan Baku Awal, dll
+<p> Berikut ini adalah varian franchise yang ada di PT.Triber Indonesia :
+	<p> 1. Kisah Romantis </p>
+		<img src ="kisahromantis.png" width="350" height="300">
+	<p> 2. Mejiku </p>
+		<img src ="mejiku.png" width="350" height="300">
+	<p> 3. Cinta Sejati </p>
+		<img src ="cintasejari.png" width="350" height="300">
+	<p> 4. Cendol Pedia </p>
+	    <img src ="cendol pedia.png" width="350" height="300"> 
+
+<?=template_footer()?>
